a3d805049777ce1d5aee7f3f1f5d4e9dd7705eda
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-30 12:58:52 +0330

    Remap the value to get a smooth effect of steam

--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -13,6 +13,9 @@ void main() {
   //=== Smoke - 01
   float smoke = texture(uTexture, smokeUv).r;
 
+  //=== Remap - 03
+  smoke = smoothstep(0.4, 1.0, smoke);
+
  //==== Final Color
   gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
 
@@ -23,7 +26,7 @@ void main() {
 
 
 
-/* ************** smoke
+/* **************  Smoke
 - switch the variable from "vec4 smoke" to a "float smoke" and only retrieve the "r" channel
 
 * since the Perlin texture is a grayscale image, we can use the "red" channel only, because it's a grayscale image and (r, g, b, a) all of them has the same value
@@ -43,6 +46,20 @@ void main() {
 ? smokeUv.x *= 0.5;
   - make the pattern bigger
   - we only pick from 0 to 0.5, half of it, instead of picking from 0 to 1
-  - we want it not to be too detailed
- 
+  - we want it not to be too detailed  */
+
+
+/* **********  Remap the value
+  - the perlin-texture pixels go from 0(black) to 1(white), which is why there are no large transparent areas within the texture
+
+  - we need to "remap" the value so that it goes from 0 when it should be 0.4, to 1 when it should be 1
+
+
+  ? smoke = smoothstep(0.4, 1.0, smoke);
+  - 0.4: where it should start
+  - 1.0: where it should end
+  - smoke: the value we want to remap
+
+  * smoothstep returns a 0 to 1 clamped value
 */
+
